57dce48819dec4eb91cbca742e0dee03dd9b84a5
iokky <58838881+iokky@users.noreply.github.com>
2023-02-12 17:39:31 +0300

    add solution

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Programm
+{
+    static void Main(string[] args)
+    {
+        string[] testArray = { "1234", "1567", "-2", "computer cience" };
+        string[] data = StringArrayCreator(testArray);
+        foreach (string item in data)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static string[] StringArrayCreator(string[] array) => array.Select(x => x).Where(x => x.Length < 4).ToArray();
+
+}
